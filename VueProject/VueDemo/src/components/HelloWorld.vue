574049bc16e522e5ddd2328ea5937322f473e881
AllongYangjian <952801974@qq.com>
2021-03-29 22:53:22 +0800

    创建项目

---
+++ VueProject/VueDemo/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <p class="msg">{{msg}}</p>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            msg:'Hello World'
+        }
+    }
+}
+</script>
+
+<style>
+ .msg{
+    color: red;
+ }
+</style>
